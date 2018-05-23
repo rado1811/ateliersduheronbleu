4c3c6b0974cfbd7318613dcacec7c9f062f95019
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-05-23 12:10:36 +0200

    database connection

--- back/app.js
+++ back/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const connection = require('./config/db.js');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -37,5 +39,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// let  server  =  app.listen( process.env.PORT  ||  5000, function(){
+//   console.log('Listening on port '  +  server.address().port);
+// });
 
 module.exports = app;
