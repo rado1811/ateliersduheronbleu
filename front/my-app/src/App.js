4848f3eb4345a38c8a8a5b9d00dfcb1ace3dde45
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-05-23 16:01:56 +0200

    modif app.js

--- front/my-app/src/App.js
+++ front/my-app/src/App.js
@@ -8,7 +8,6 @@ class App extends Component {
         <header className="App-header">
             <h1 className="App-title">Les ateliers du héron bleu</h1>
         </header>
-
       </div>
     );
   }
