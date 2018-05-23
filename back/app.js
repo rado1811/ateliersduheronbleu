6d678674cd152b4cd92c68c1590d217f6e52ab1c
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-05-23 12:18:17 +0200

    msg connection port

--- back/app.js
+++ back/app.js
@@ -40,8 +40,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// let  server  =  app.listen( process.env.PORT  ||  5000, function(){
-//   console.log('Listening on port '  +  server.address().port);
-// });
-
 module.exports = app;
