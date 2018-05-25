6632734f21c0f30944e8d1eaf8ea58c7d5f9c5a2
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-05-25 09:44:02 +0200

    integration material ui sur affichage cartes homepage

--- front/my-app/src/App.js
+++ front/my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import AtelierHome from './Components/Client/ateliersHome/atelierHome';
 
 const App = () => (
   <div className="App">
     <div className="App-header">
-      <h2>Atelier du Heron bleu</h2>
+      <h2>Ateliers du Heron bleu</h2>
     </div>
+    <AtelierHome />
   </div>
 );
 
---
+++ front/my-app/src/Components/Client/ateliersHome/atelierVignette.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import './atelierVignette.css';
+
+const styles = {
+  item: {
+    margin: '20px',
+  },
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+};
+
+const AtelierVignette = (props) => {
+  const { classes } = props;
+
+  return (
+    <Grid item md={4} sm={12}>
+      <Card className={classes.card} justify="center">
+        <CardMedia
+          className={classes.media}
+          image={props.image}
+          title="Héron"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="headline" component="h2">
+          {props.name}
+          </Typography>
+          <Typography component="h3">
+            {props.date}
+          </Typography>
+          <Typography component="h3">
+            Co-animation: {props.intervenant}, Isabelle Jono
+          </Typography>
+
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary" onClick={props.booking}>
+            Pré-réserver
+          </Button>
+          <Button size="small" color="primary" onClick={props.moreDetails}>
+            En savoir plus
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+
+  )
+};
+
+export default withStyles(styles)(AtelierVignette);
