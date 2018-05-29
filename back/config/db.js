6428f1feb6e0b0f781ea1bafb3daf6b908c47a05
Thibaut C <thibaut.cointet@gmail.com>
2018-05-29 15:26:04 +0200

    Chevrons delete

--- back/config/db.js
+++ back/config/db.js
@@ -5,17 +5,14 @@ const connection = mysql.createConnection({
   host: 'sql7.freemysqlhosting.net',
   user: 'sql7239441',
   password: 'lI1KZJjzVK',
-<<<<<<< HEAD
   database: 'sql7239441'
 });
 
 connection.connect((err) => {
-=======
   database: 'sql7239441',
 });
 
 connection.connect(function(err) {
->>>>>>> navBar
   if (!err) {
     console.log('Database connected');
   } else {
