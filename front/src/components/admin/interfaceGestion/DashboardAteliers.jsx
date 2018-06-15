7087ecc4fb19a4c8ca5dd72cf750ad4772dda951
Thibaut C <thibaut.cointet@gmail.com>
2018-06-15 10:37:49 +0200

    key dans map

--- front/src/components/admin/interfaceGestion/DashboardAteliers.jsx
+++ front/src/components/admin/interfaceGestion/DashboardAteliers.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,61 +6,51 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-
 class DashboardAteliers extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ateliers: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      ateliers: [],
+    };
+  }
 
-    componentDidMount(){
-        fetch("/api/ateliers")
-        .then(res => res.json())
-        .then(ateliers => this.setState({ ateliers }));
-    }
+  componentDidMount() {
+    fetch('/api/ateliers')
+      .then(res => res.json())
+      .then(ateliers => this.setState({ ateliers }));
+  }
 
-    render() {
-        return (
-            <div>
-            <h1 className="text-center">Tableau de bord des ateliers</h1>
-            <Paper>
-                <Table style={{marginTop:50}} >
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Nom de l'atelier</TableCell>
-                            <TableCell numeric>Places prevus</TableCell>
-                            <TableCell numeric>Personnes inscrites</TableCell>
-                            <TableCell numeric>Places restantes</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-
-                            {this.state.ateliers.map(atelier => {
-                                return (
-                                    <TableRow key={atelier.id}>
-                                    <TableCell component="th" scope="row">
-                                    {atelier.nom}
-                                    </TableCell>
-                                    <TableCell numeric>{atelier.nb_participants}</TableCell>
-                                    <TableCell numeric></TableCell>
-                                    <TableCell numeric>{atelier.place_disponible}</TableCell>
-                                    </TableRow>
-
-                                );                               
-                            })
-                            }
-                    </TableBody>
-                </Table>
-            </Paper>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">Tableau de bord des ateliers</h1>
+        <Paper>
+          <Table style={{ marginTop: 50 }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Nom de l'atelier</TableCell>
+                <TableCell numeric>Places prevus</TableCell>
+                <TableCell numeric>Personnes inscrites</TableCell>
+                <TableCell numeric>Places restantes</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.ateliers.map((atelier) => (
+                <TableRow key={atelier.id_atelier} >
+                  <TableCell component="th" scope="row">
+                    {atelier.nom}
+                  </TableCell>
+                  <TableCell numeric>{atelier.nb_participants}</TableCell>
+                  <TableCell numeric />
+                  <TableCell numeric>{atelier.place_disponible}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    );
+  }
 }
-
-DashboardAteliers.propTypes = {
-    classes: PropTypes.shape.isRequired,
-};
 
 export default DashboardAteliers;
