da03b4d4fc0d5355665ea92674ec19495e2fac02
Thibaut C <thibaut.cointet@gmail.com>
2018-06-15 09:56:42 +0200

    ready to merge

--- back/routes/intervenant.js
+++ back/routes/intervenant.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/intervenant', (req, res, next) => {
   connection.query('SELECT * FROM Intervenants', (err, data) => {
     if (err) {
-      res.send(err)
+      res.send(err);
     } else {
       res.json(data);
     }
@@ -16,7 +16,7 @@ router.get('/intervenant', (req, res, next) => {
 router.post('/intervenant', (req, res) => {
   const sql = 'INSERT INTO Intervenants SET ?';
   connection.query(sql, req.body, (err, result) => {
-    if (err) { console.log(err);
+    if (err) {
       throw err;
     }
     res.send(`${result.insertId}`);
@@ -24,13 +24,12 @@ router.post('/intervenant', (req, res) => {
 });
 
 router.delete('/intervenant/:id', (req, res) => {
-  const deletedIntervenant = `DELETE FROM Intervenants WHERE id=${req.params.id}`
-  connection.query(deletedIntervenant, function (err, rows) {
-    if (err)
-      throw err;
+  const deletedIntervenant = `DELETE FROM Intervenants WHERE id=${
+    req.params.id
+  }`;
+  connection.query(deletedIntervenant, function(err, rows) {
+    if (err) throw err;
   });
 });
 
-
 export default router;
-
