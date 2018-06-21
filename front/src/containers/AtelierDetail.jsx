3420617258dec0065394e6869c4c68dd409cdd8e
rado1811 <36564887+rado1811@users.noreply.github.com>
2018-06-21 10:21:34 +0200

    fetch pageAtelier OK

--- front/src/containers/AtelierDetail.jsx
+++ front/src/containers/AtelierDetail.jsx
@@ -1,74 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchAteliers } from '../actions/ateliers';
+import PropTypes from 'prop-types';
 import Grid from 'material-ui/Grid';
-
 import './AteliersDetails.css';
 
-const AteliersDetail = (props) => {
-  const { ateliers } = props;
-  return (
-    <div className="AteliersDetail">
-      {!ateliers ? (
-        <div>
-          <h3>Les Ateliers du Héron Bleu</h3>
-          Choisissez un atelier
-        </div>
-      ) : (
-        <div>
-          <Grid
-            container
-            style={{
-              height: '100%',
-            }}
-          >
-            <Grid className="details" item md={6} xs={1}>
-              <div>
-                <img src={ateliers.image} alt="heron" />
-              </div>
+class AtelierDetail extends Component {
+  componentDidMount() {
+    this.props.fetchAteliers();
+  }
+  render() {
+    return (
+      <div className="AteliersDetail">
+        {!this.props.ateliers ? (
+          <div>
+            <h3>Les Ateliers du Héron Bleu</h3>
+            Choisissez un atelier
+          </div>
+        ) : (
+          <div>
+            <Grid
+              container
+              style={{
+                height: '100%',
+              }}
+            >
+              <Grid className="details" item md={6} xs={1}>
+                <div>
+                  <img src={this.props.ateliers.image} alt="heron" />
+                </div>
+              </Grid>
+              <Grid className="contenu" item md={6} xs={1} key={this.props.ateliers.key}>
+                <div>
+                  <h3>{this.props.ateliers.nom}</h3>
+                  <h4>{this.props.ateliers.debut}</h4>
+                  <b>
+                    <img
+                      src="https://cdn4.iconfinder.com/data/icons/birds-1/154/bird-stork-heron-pelican-512.png"
+                      width="5%"
+                      alt="heron"
+                    />
+                    En chemin, vous trouverez :
+                  </b>
+                  <p>{this.props.ateliers.chemin}</p>
+                  <b>
+                    <img
+                      src="https://cdn4.iconfinder.com/data/icons/birds-1/154/bird-stork-heron-pelican-512.png"
+                      width="5%"
+                      alt="iconHeron"
+                    />
+                    Programme :
+                  </b>
+                  <p>{this.props.ateliers.programme}</p>
+                  <b>
+                    <img
+                      src="https://cdn4.iconfinder.com/data/icons/birds-1/154/bird-stork-heron-pelican-512.png"
+                      width="5%"
+                      alt="iconHeron"
+                    />
+                    Intervenants :
+                  </b>
+                  <p>{this.props.ateliers.intervenants}</p>
+                </div>
+              </Grid>
             </Grid>
-            <Grid className="contenu" item md={6} xs={1} key={ateliers.key}>
-              <div>
-                <h3>{ateliers.nom}</h3>
-                <h4>{ateliers.debut}</h4>
-                <b>
-                  <img
-                    src="https://cdn4.iconfinder.com/data/icons/birds-1/154/bird-stork-heron-pelican-512.png"
-                    width="5%"
-                    alt="heron"
-                  />
-                  En chemin, vous trouverez :
-                </b>
-                <p>{ateliers.chemin}</p>
-                <b>
-                  <img
-                    src="https://cdn4.iconfinder.com/data/icons/birds-1/154/bird-stork-heron-pelican-512.png"
-                    width="5%"
-                    alt="iconHeron"
-                  />
-                  Programme :
-                </b>
-                <p>{ateliers.programme}</p>
-                <b>
-                  <img
-                    src="https://cdn4.iconfinder.com/data/icons/birds-1/154/bird-stork-heron-pelican-512.png"
-                    width="5%"
-                    alt="iconHeron"
-                  />
-                  Intervenants :
-                </b>
-                <p>{ateliers.intervenants}</p>
-              </div>
-            </Grid>
-          </Grid>
-        </div>
-      )};
-    </div>
-  );
+          </div>
+        )};
+      </div>
+    );
+  };
+}
+
+AtelierDetail.propTypes = {
+  fetchAteliers: PropTypes.func.isRequired,
 };
 
-// "state.activeUser" is set in reducers/index.js
 function mapStateToProps(state) {
   return { ateliers: state.activeAteliers };
 }
 
-export default connect(mapStateToProps)(AteliersDetail);
+export default connect(mapStateToProps, { fetchAteliers })(AtelierDetail);
