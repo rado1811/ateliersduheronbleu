c83afc6957cea29333d62d5a0fb4b5d573e37755
Thibaut C <thibaut.cointet@gmail.com>
2018-07-05 23:22:25 +0200

    CSS Formulaires Add/Edit Atelier/Intervenant : add Paper + marginTop

--- front/src/components/admin/formulaire/atelier/formulaireAtelier/FormAtelier.jsx
+++ front/src/components/admin/formulaire/atelier/formulaireAtelier/FormAtelier.jsx
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
+import Paper from '@material-ui/core/Paper';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Place from '@material-ui/icons/Place';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -176,160 +177,188 @@ class FormAtelier extends Component {
   render() {
     const { isFromEdit } = this.props;
     return (
-      <Grid>
-        <div>
-          <h1 className="text-center">Ajouter un Atelier</h1>
-          <form onSubmit={isFromEdit ? this.handleUpdate : this.handleSubmit}>
-            <Grid container spacing={24}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="nom"
-                  required
-                  label="Titre de l'Atelier"
-                  type="text"
-                  value={this.state.nom}
-                  onChange={this.updateNomField}
-                />
-                <br />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <br />
-                <TextField
-                  name="debut"
-                  required
-                  label=""
-                  type="date"
-                  value={this.state.debut}
-                  onChange={this.updateDebutField}
-                />
-                <br />
-              </Grid>
-            </Grid>
-            <Grid container spacing={24}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="nb_participants"
-                  required
-                  label="Nombre Participants :"
-                  type="number"
-                  value={this.state.nb_participants}
-                  onChange={this.updateNbField}
-                />
-                <br />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  name="prix"
-                  required
-                  label="Prix"
-                  type="text"
-                  value={this.state.prix}
-                  onChange={this.updatePrixField}
-                />
-                <br />
-              </Grid>
-            </Grid>
-            <TextField
-              fullWidth
-              multiline
-              name="contenu"
-              required
-              label="Contenu"
-              multiligne="true"
-              type="text"
-              value={this.state.contenu}
-              onChange={this.updateContenuField}
-            />
-            <br />
-            <br />
-            <TextField
-              name="formule"
-              label="Formule ?"
-              required
-              type="text"
-              value={this.state.formule}
-              onChange={this.updateFormuleField}
-            />
-            <br />
-            <TextField
-              name="photo"
-              label="Photo"
-              value={this.state.photo}
-              onChange={this.updatePhotoField}
-            />
-            <br />
-            <br />
-            <TextField
-              name="lieu"
-              label=""
-              type="text"
-              value={this.state.lieu}
-              onChange={this.updateLieuField}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Place />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <br />
-            <br />
-            <InputLabel htmlFor="dropInput">Intervenant</InputLabel>
-            <Select
-              value={this.state.nom_intervenant}
-              onChange={this.updateIntervenantField}
-            >
-              <MenuItem value="">
-                <em>Selectionnez un intervenant</em>
-              </MenuItem>
-              {this.props.intervenants.map((item) => (
-                <MenuItem key={item.id_intervenant} value={item.id_intervenant}>
-                  {item.nom} {item.prenom}
-                </MenuItem>
-              ))}
-            </Select>
-            <br />
-            <TextField
-              fullWidth
-              multiline
-              name="programme"
-              label="Programme"
-              type="text"
-              value={this.state.programme}
-              onChange={this.updateProgrammeField}
-            />
-            <br />
-            <br />
-            <div>
+      <Grid
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: 80,
+        }}
+      >
+        <Paper style={{ padding: 20 }} elevation={24}>
+          <div>
+            <form onSubmit={isFromEdit ? this.handleUpdate : this.handleSubmit}>
               {isFromEdit ? (
-                <Button
-                  type="submit"
-                  value="Submit"
-                  variant="raised"
-                  color="primary"
-                >
-                  Modifier
-                </Button>
+                <h1 style={{ textAlign: 'center' }}>
+                  Modification d'un atelier
+                </h1>
               ) : (
-                <Button
-                  type="submit"
-                  value="Submit"
-                  variant="raised"
-                  color="primary"
-                >
-                  Enregistrer
-                </Button>
+                <h1 style={{ textAlign: 'center' }}>Ajouter un atelier</h1>
               )}
-            </div>
-          </form>
-          <Snackbar
-            open={this.state.open}
-            message={this.state.flash}
-            autoHideDuration={4000}
-            onClose={this.handleToogle}
-          />
-        </div>
+              <Grid container spacing={24}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    style={{ margin: 15 }}
+                    name="nom"
+                    required
+                    label="Titre de l'Atelier"
+                    type="text"
+                    value={this.state.nom}
+                    onChange={this.updateNomField}
+                  />
+                  <br />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <br />
+                  <TextField
+                    style={{ margin: 15 }}
+                    name="debut"
+                    required
+                    label=""
+                    type="date"
+                    value={this.state.debut}
+                    onChange={this.updateDebutField}
+                  />
+                  <br />
+                </Grid>
+              </Grid>
+              <Grid container spacing={24}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    style={{ margin: 15 }}
+                    name="nb_participants"
+                    required
+                    label="Nombre Participants :"
+                    type="number"
+                    value={this.state.nb_participants}
+                    onChange={this.updateNbField}
+                  />
+                  <br />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    style={{ margin: 15 }}
+                    name="prix"
+                    required
+                    label="Prix"
+                    type="text"
+                    value={this.state.prix}
+                    onChange={this.updatePrixField}
+                  />
+                  <br />
+                </Grid>
+              </Grid>
+              <TextField
+                style={{ margin: 15 }}
+                fullWidth
+                multiline
+                name="contenu"
+                required
+                label="Contenu"
+                multiligne="true"
+                type="text"
+                value={this.state.contenu}
+                onChange={this.updateContenuField}
+              />
+              <br />
+              <br />
+              <TextField
+                style={{ margin: 15 }}
+                name="formule"
+                label="Formule ?"
+                required
+                type="text"
+                value={this.state.formule}
+                onChange={this.updateFormuleField}
+              />
+              <br />
+              <TextField
+                style={{ margin: 15 }}
+                name="photo"
+                label="Photo"
+                value={this.state.photo}
+                onChange={this.updatePhotoField}
+              />
+              <br />
+              <br />
+              <TextField
+                style={{ margin: 15 }}
+                name="lieu"
+                label=""
+                type="text"
+                value={this.state.lieu}
+                onChange={this.updateLieuField}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Place />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <br />
+              <br />
+              <InputLabel htmlFor="dropInput">Intervenant</InputLabel>
+              <Select
+                value={this.state.nom_intervenant}
+                onChange={this.updateIntervenantField}
+              >
+                <MenuItem value="">
+                  <em>Selectionnez un intervenant</em>
+                </MenuItem>
+                {this.props.intervenants.map((item) => (
+                  <MenuItem
+                    key={item.id_intervenant}
+                    value={item.id_intervenant}
+                  >
+                    {item.nom} {item.prenom}
+                  </MenuItem>
+                ))}
+              </Select>
+              <br />
+              <TextField
+                style={{ margin: 15 }}
+                fullWidth
+                multiline
+                name="programme"
+                label="Programme"
+                type="text"
+                value={this.state.programme}
+                onChange={this.updateProgrammeField}
+              />
+              <br />
+              <br />
+              <div>
+                {isFromEdit ? (
+                  <Button
+                    style={{ margin: 15 }}
+                    type="submit"
+                    value="Submit"
+                    variant="raised"
+                    color="primary"
+                  >
+                    Modifier
+                  </Button>
+                ) : (
+                  <Button
+                    style={{ margin: 15 }}
+                    type="submit"
+                    value="Submit"
+                    variant="raised"
+                    color="primary"
+                  >
+                    Enregistrer
+                  </Button>
+                )}
+              </div>
+            </form>
+            <Snackbar
+              open={this.state.open}
+              message={this.state.flash}
+              autoHideDuration={4000}
+              onClose={this.handleToogle}
+            />
+          </div>
+        </Paper>
       </Grid>
     );
   }
